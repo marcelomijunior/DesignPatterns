0872b0b1d0fc573f07d681d7b9225f47fe5f8c1f
Marcelo de Miranda Junior <marcelomijunior@gmail.com>
2022-08-03 08:47:35 -0300

    Adicionar arquivos de projeto.

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Curso de Design Patterns");
+Console.WriteLine("");
+
+Console.WriteLine(@"Tecle ""Enter"" para finalizar a aplicação...");
+Console.ReadKey();
